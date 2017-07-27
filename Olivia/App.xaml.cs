a8bc8c731ba8c92140524edaccafeffe454bf6b5
EdsonAF <edsonsintjago@gmail.com>
2017-07-27 03:37:05 +0200

    scafolding

--- Olivia/App.xaml.cs
+++ Olivia/App.xaml.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Olivia
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    protected override void OnStartup(StartupEventArgs e)
     {
+      base.OnStartup(e); MainWindow window = new MainWindow();
+      
+      // Create the ViewModel to which 
+      // the main window binds. 
+      var viewModel = new MainWindowViewModel();
+
+      // Allow all controls in the window to 
+      // bind to the ViewModel by setting the 
+      // DataContext, which propagates down 
+      // the element tree. 
+      window.DataContext = viewModel;
+      window.Show();
     }
+  }
 }
---
+++ Olivia/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+
+namespace Olivia
+{
+  public class MainWindowViewModel
+  {
+    public MainWindowViewModel()
+    {
+    }
+
+    public string InputFolderPath { get; set; }
+
+    public string OutputFolderPath { get; set; }
+    
+    private RelayCommand _browseFolderCommand;
+    public ICommand BrowseFolderCommand
+    {
+      get
+      {
+        return _browseFolderCommand ?? (_browseFolderCommand = new RelayCommand(param =>
+        {
+
+        }));
+      }
+    }
+  }
+}
